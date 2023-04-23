d393d78a7bf335e5f07237a551b9f79f03ac3697
Reimund König <reimund@gmx.net>
2023-04-23 14:10:45 +0200

    remove 1 min delay bug

--- neolamp/src/lamphelper.cpp
+++ neolamp/src/lamphelper.cpp
@@ -19,12 +19,13 @@ Clocktime LampHelper::get_biggest_time(Clocktime t1, Clocktime t2,
     return get_biggest_time(tmp, t3);
 }
 
-bool LampHelper::is_t1_greater_t2(Clocktime t1, Clocktime t2) {
+bool LampHelper::is_t1_greater_or_euqal_t2(Clocktime t1, Clocktime t2) {
     if(t1.getHour() > t2.getHour()) { return true; }
     if(t1.getHour() < t2.getHour()) { return false; }
-    if(t1.getMinutes() > t2.getMinutes()) { return true; }
+    if(t1.getMinutes() >= t2.getMinutes()) { return true; }
     return false;
 }
+
 bool LampHelper::is_time_equal(Clocktime t1, Clocktime t2) {
     if(t1.getHour() == t2.getHour() && t1.getMinutes() == t2.getMinutes()) {
         return true;
@@ -37,15 +38,15 @@ int LampHelper::get_mode(Clocktime current_time, Clocktime t1, int m1,
     Clocktime big_t = get_biggest_time(t1, t2, t3);
     if(is_time_equal(big_t, t1)) {
         // t1 is the biggest time
-        if(is_t1_greater_t2(current_time, t1)) {
+        if(is_t1_greater_or_euqal_t2(current_time, t1)) {
             return m1;
         } else {
             big_t = get_biggest_time(t2, t3);
             if(is_time_equal(big_t, t2)) {
                 // t2 is the second biggest time
-                if(is_t1_greater_t2(current_time, t2)) {
+                if(is_t1_greater_or_euqal_t2(current_time, t2)) {
                     return m2;
-                } else if(is_t1_greater_t2(current_time, t3)) {
+                } else if(is_t1_greater_or_euqal_t2(current_time, t3)) {
                     return m3;
                 } else {
                     // current time is smaller then all times
@@ -55,15 +56,15 @@ int LampHelper::get_mode(Clocktime current_time, Clocktime t1, int m1,
         }
     } else if(is_time_equal(big_t, t2)) {
         // t2 is the biggest time
-        if(is_t1_greater_t2(current_time, t2)) {
+        if(is_t1_greater_or_euqal_t2(current_time, t2)) {
             return m2;
         } else {
             big_t = get_biggest_time(t1, t3);
             if(is_time_equal(big_t, t1)) {
                 // t1 is the second biggest time
-                if(is_t1_greater_t2(current_time, t1)) {
+                if(is_t1_greater_or_euqal_t2(current_time, t1)) {
                     return m1;
-                } else if(is_t1_greater_t2(current_time, t3)) {
+                } else if(is_t1_greater_or_euqal_t2(current_time, t3)) {
                     return m3;
                 } else {
                     // current time is smaller then all times
@@ -73,15 +74,15 @@ int LampHelper::get_mode(Clocktime current_time, Clocktime t1, int m1,
         }
     } else if(is_time_equal(big_t, t3)) {
         // t3 is the biggest time
-        if(is_t1_greater_t2(current_time, t3)) {
+        if(is_t1_greater_or_euqal_t2(current_time, t3)) {
             return m3;
         } else {
             big_t = get_biggest_time(t1, t2);
             if(is_time_equal(big_t, t1)) {
                 // t1 is the second biggest time
-                if(is_t1_greater_t2(current_time, t1)) {
+                if(is_t1_greater_or_euqal_t2(current_time, t1)) {
                     return m1;
-                } else if(is_t1_greater_t2(current_time, t2)) {
+                } else if(is_t1_greater_or_euqal_t2(current_time, t2)) {
                     return m2;
                 } else {
                     // current time is smaller then all times
