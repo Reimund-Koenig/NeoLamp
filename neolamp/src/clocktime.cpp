b8c3010c31c1f71d3b42bd1b33603853e2dc21d4
Reimund König <reimund@gmx.net>
2023-04-23 13:54:05 +0200

    refactor and add learning mode

--- neolamp/src/clocktime.cpp
+++ neolamp/src/clocktime.cpp
@@ -29,35 +29,22 @@ String getValue(String data, char separator, int index) {
 
 boolean Clocktime::isValidNumber(String str) {
     for(byte i = 0; i < str.length(); i++) {
-        if(!isDigit(str.charAt(i)))
-            return false;
+        if(!isDigit(str.charAt(i))) { return false };
     }
     return true;
 }
 
 bool Clocktime::setTime(String timestring) {
-    if(timestring.length() < 3 || timestring.length() > 5) {
-        return false;
-    }
+    if(timestring.length() < 3 || timestring.length() > 5) { return false; }
     String timestring_minutes = getValue(timestring, ':', 1);
     String timestring_hours = getValue(timestring, ':', 0);
-    if(timestring_hours == "" || timestring_minutes == "") {
-        return false;
-    }
-    if(!isValidNumber(timestring_minutes)) {
-        return false;
-    }
-    if(!isValidNumber(timestring_hours)) {
-        return false;
-    }
+    if(timestring_hours == "" || timestring_minutes == "") { return false; }
+    if(!isValidNumber(timestring_minutes)) { return false; }
+    if(!isValidNumber(timestring_hours)) { return false; }
     int tmp_min = timestring_minutes.toInt();
     int tmp_hour = timestring_hours.toInt();
-    if(tmp_hour < 0 || tmp_hour > 24) {
-        return false;
-    }
-    if(tmp_min < 0 || tmp_min > 60) {
-        return false;
-    }
+    if(tmp_hour < 0 || tmp_hour > 24) { return false; }
+    if(tmp_min < 0 || tmp_min > 60) { return false; }
     this->minutes = tmp_min;
     this->hours = tmp_hour;
     return true;
@@ -67,14 +54,10 @@ void Clocktime::print() { Serial.println(Clocktime::getTimeString()); }
 
 String Clocktime::getTimeString() {
     String tmp = "";
-    if(hours < 10) {
-        tmp += "0";
-    }
+    if(hours < 10) { tmp += "0"; }
     tmp += String(hours);
     tmp += ":";
-    if(minutes < 10) {
-        tmp += "0";
-    }
+    if(minutes < 10) { tmp += "0"; }
     tmp += String(minutes);
     tmp += " Uhr";
     return tmp;
--- neolamp/src/constants/modes.h
+++ neolamp/src/constants/modes.h
@@ -2,4 +2,5 @@ const char *array_of_modes[][2] = {{"Farbkeise und Pulsieren", "mix"},
                                    {"Pulsieren", "pulse"},
                                    {"Farbkeise", "circle"},
                                    {"Gruen", "green"},
-                                   {"Lampe Ausgeschaltet", "off"}};
+                                   {"Lampe Ausgeschaltet", "off"},
+                                   {"Lernmodus", "learn"}};
