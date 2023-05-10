20619a2d09128813e2b7fcecc5370a0546830854
Reimund König <reimund@gmx.net>
2023-05-10 21:27:38 +0200

    add script to update Arduino ESP Async Wifi Manager

--- ESPAsyncWifiManager/update_async_wifi_mgmr.sh
+++
@@ -1,6 +0,0 @@
-echo 'const char index_html[] PROGMEM = R"rawliteral(' > tmp_index.html.h
-cat index.html >> tmp_index.html.h
-echo ')rawliteral";' >> tmp_index.html.h
-mv tmp_index.html.h ./neolamp/src/constants/index.html.h
-
-C:\Users\katha\Documents\Arduino\libraries\ESPAsyncWiFiManager\src
