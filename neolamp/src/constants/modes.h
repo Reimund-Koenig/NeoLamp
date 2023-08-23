d820cd7ed24826b28dcc848b47758f3ce356ab39
Reimund <reimund.koenig@bosch.com>
2023-08-23 07:33:14 +0200

    fix typos

--- neolamp/src/constants/modes.h
+++ neolamp/src/constants/modes.h
@@ -1,11 +1,11 @@
 #define STATE_ANIMATION_MIX "mix"
 #define STATE_ANIMATION_PULSE "pulse"
-#define STATE_ANIMATION_CIRCLE "circle")
-#define STATE_ANIMATION_RAINBOW "rainbow")
-#define STATE_ANIMATION_PICK "pick")
-#define STATE_ANIMATION_GREEN "green")
-#define STATE_ANIMATION_RED "orange")
-#define STATE_ANIMATION_OFF "off")
+#define STATE_ANIMATION_CIRCLE "circle"
+#define STATE_ANIMATION_RAINBOW "rainbow"
+#define STATE_ANIMATION_PICK "pick"
+#define STATE_ANIMATION_GREEN "green"
+#define STATE_ANIMATION_RED "orange"
+#define STATE_ANIMATION_OFF "off"
 
 const char *array_of_modes[][2] = {
     {"Mixed", STATE_ANIMATION_MIX},
