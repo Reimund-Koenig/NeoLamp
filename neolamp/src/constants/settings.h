2bc90220e35a17ccaebb5f8b5528ba789710b82b
Reimund König <reimund@gmx.net>
2023-10-06 20:21:19 +0200

    fix gpio port descr.

--- neolamp/src/constants/settings.h
+++ neolamp/src/constants/settings.h
@@ -1,6 +1,6 @@
 #define NEOPIXEL_PIN 4 // Wemos D1 mini: port D2
 #define LED_BLUE 0     // Wemos D1 mini: port D3
-#define LED_YELLOW 2   // Wemos D1 mini: port D3
+#define LED_YELLOW 2   // Wemos D1 mini: port D4
 
 #define NAME "Nachtlicht"
 #define URL "nachtlicht"
