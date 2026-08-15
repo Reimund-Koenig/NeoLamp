5384a3af2a12b6a2e3a2b3004ff55a2d9b37b9c2
Reimund König <reimund@gmx.net>
2026-08-15 13:00:35 +0200

    set name to original

--- neolamp/src/constants/settings.h
+++ neolamp/src/constants/settings.h
@@ -2,8 +2,8 @@
 #define FILENAME_VERSION "NeoLamp_v1.1.0.bin"
 
 // Change this names to your need
-#define NAME "Bella"
-#define URL "bella"
+#define NAME "Nachtlicht"
+#define URL "nachtlicht"
 #define NEOPIXEL_PIN 4 // Wemos D1 mini: port D2
 #define NEOPIXEL_COUNT 16
 
